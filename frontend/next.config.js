7c217c7bac1ee5c65d3da9d0df16246b2e4f5c88
Francesito <103217923+Francesito@users.noreply.github.com>
2025-07-17 13:04:49 -0600

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,20 @@
-// frontend/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Configuración para Render
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
