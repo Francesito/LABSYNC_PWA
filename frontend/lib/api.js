df3a5972bcd20ca23e77a3062849cc12439be2c4
Francesito <103217923+Francesito@users.noreply.github.com>
2025-07-17 15:22:23 -0600

    Update api.js

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,  // <-- quitar el "/api" extra
+  baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api`, 
 });
 
 // --- Solicitudes (alumno/docente) ---
