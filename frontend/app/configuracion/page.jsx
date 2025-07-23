823330bf9911f87a6036def4087c14e4328683e8
Francesito <103217923+Francesito@users.noreply.github.com>
2025-07-23 13:27:50 -0600

    Update page.jsx

--- frontend/app/configuracion/page.jsx
+++ frontend/app/configuracion/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { useAuth } from '../lib/auth';
+import { useAuth } from '../../lib/auth'; // Corregido de '../lib/auth' a '../../lib/auth'
 import axios from 'axios';
 
 export default function Configuracion() {
